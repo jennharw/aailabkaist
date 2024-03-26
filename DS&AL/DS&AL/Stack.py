006f4a0ce245834e89a6907fec98fe8dea087f40
harim <holly001503@gmail.com>
2024-03-27 07:33:17 +0900

    first commit

--- DS&AL/DS&AL/Stack.py
+++
@@ -1,9 +0,0 @@
-from SinglyLinkedList import SinglyLinkedList
-
-class Stack(object):
-    lstInstance = SinglyLinkedList()
-
-    def pop(self):
-        return self.lstInstance.removeAt(0)
-    def push(self, value):
-        self.lstInstance.insertAt(value,0)
--- ML/gradient_Logistic.py
+++
@@ -1,7 +0,0 @@
-import scipy.io as sio
-data = sio.loadmat('sentimentdataset.mat')
-bagofword = data['bagofword']
-sentiment = data['sentiment']
-sentiment = sentiment.astype(int)
-words = data['word']
-
